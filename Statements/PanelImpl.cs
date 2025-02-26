ae4a11755866d35ea21e9f7dd7c274e49ef9616e
SubstanceP <3161387475@qq.com>
2025-02-26 23:59:55 +0800

    feat: Show/Hide Text

---
+++ Statements/PanelImpl.cs
@@ -0,0 +1,118 @@
+﻿using Avalonia.Animation;
+using StoryTable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Media;
+using Avalonia.Styling;
+using Avalgame.Views;
+using Avalonia.Controls;
+using Avalonia.Animation.Easings;
+
+namespace Avalgame.Statements
+{
+    public partial class PanelStatement : Statement
+    {
+        public enum Option
+        {
+            Text,
+        }
+        private static Dictionary<Option, PanelAnim>? panels;
+        private static Dictionary<Option, PanelAnim> Panels => panels ??= new()
+        {
+            { Option.Text, Text },
+        };
+
+        private static readonly PanelAnim Text = new()
+        {
+            Target = GamePageView.Instance!.TextCanv,
+            #region ShowDialog
+            Show = new()
+            {
+                Anim = new Animation
+                {
+                    Duration = TimeSpan.FromSeconds(3),
+                    IterationCount = new(1),
+                    Children =
+                    {
+                        new KeyFrame
+                        {
+                            Cue = new(0),
+                            Setters =
+                            {
+                                new Setter(TranslateTransform.YProperty,
+                                    GamePageView.Instance !.NameCanv.Height +
+                                    GamePageView.Instance !.DialogCanv.Height),
+                                new Setter(Avalonia.Visual.OpacityProperty, 0.0),
+                            }
+                        },
+                        new KeyFrame
+                        {
+                            Cue= new(1),
+                            Setters =
+                            {
+                                new Setter(TranslateTransform.YProperty, 0.0),
+                                new Setter(Avalonia.Visual.OpacityProperty, 1.0),
+                            },
+                            KeySpline = new(.4, 0, .2, 1)
+                        }
+                    }
+                },
+                Start = target =>
+                {
+                    target.RenderTransform = new TranslateTransform(0, 0);
+                },
+                End = target =>
+                {
+                    target.Opacity = 1.0;
+                }
+            },
+            #endregion
+            #region HideDialog
+            Hide = new()
+            {
+                Anim = new Animation
+                {
+                    Duration = TimeSpan.FromSeconds(3),
+                    IterationCount = new(1),
+                    Children =
+                    {
+                        new KeyFrame
+                        {
+                            Cue = new(0),
+                            Setters =
+                            {
+                                new Setter(TranslateTransform.YProperty, 0.0),
+                                new Setter(Avalonia.Visual.OpacityProperty, 1.0),
+                            }
+                        },
+                        new KeyFrame
+                        {
+                            Cue= new(1),
+                            Setters =
+                            {
+                                new Setter(TranslateTransform.YProperty,
+                                    GamePageView.Instance!.NameCanv.Height +
+                                    GamePageView.Instance!.DialogCanv.Height),
+                                new Setter(Avalonia.Visual.OpacityProperty, 0.0),
+                            },
+                            KeySpline = new(.4, 0, .2, 1)
+                        }
+                    }
+                },
+                Start = target =>
+                {
+                    target.RenderTransform = new TranslateTransform(0,
+                        GamePageView.Instance!.NameCanv.Height + GamePageView.Instance!.DialogCanv.Height);
+                },
+                End = target =>
+                {
+                    target.Opacity = 0.0;
+                }
+            }
+            #endregion
+        };
+    }
+}
---
+++ Statements/PanelStatement.cs
@@ -0,0 +1,74 @@
+﻿using Avalgame.Views;
+using Avalonia.Animation;
+using Avalonia.Controls;
+using Avalonia.Rendering.Composition.Animations;
+using Avalonia.Threading;
+using StoryTable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Avalgame.Statements
+{
+    [Statement("SHOW")]
+    public class ShowStatement : PanelStatement
+    {
+        public ShowStatement(ArgParser parser) : base(parser) { Reverse = false; }
+    }
+    [Statement("HIDE")]
+    public class HideStatement : PanelStatement
+    {
+        public HideStatement(ArgParser parser) : base(parser) { Reverse = true; }
+    }
+
+    public partial class PanelStatement : Statement
+    {
+        protected bool Reverse;
+
+        private readonly Option option;
+        public PanelStatement(ArgParser parser) : base(parser)
+        {
+            option = parser.Enum<Option>();
+        }
+        public override ExecuteMode Mode => ExecuteMode.Wait;
+        public async override void Execute(ExecutorBase executor)
+        {
+            var panel = Panels[option];
+            var info = Reverse ? panel.Hide : panel.Show;
+            var anim = info.Anim;
+            var cts = new CancellationTokenSource();
+
+            info.Start(panel.Target);
+            _ = anim.RunAsync(panel.Target, cts.Token);
+            int refreshTime = (executor as ExecutorImpl)!.RefreshTime;
+            int countdown = (int)anim.IterationCount.Value * (int)anim.Duration.TotalMilliseconds;
+            while ((countdown -= refreshTime) > 0 && !executor.Skip) await Task.Delay(refreshTime);
+
+            cts.Cancel();
+            info.End(panel.Target);
+            executor.Complete();
+        }
+    }
+
+    public class PanelAnim
+    {
+        public Control Target;
+        public AnimInfo Show;
+        public AnimInfo Hide;
+        public class AnimInfo
+        {
+            public Animation Anim;
+            /// <summary>
+            /// 预处理<see cref="Target"/防止结尾跳变
+            /// </summary>
+            public Action<Control> Start;
+            /// <summary>
+            /// 设置<see cref="Target"/>结束时状态
+            /// </summary>
+            public Action<Control> End;
+        }
+    }
+}
