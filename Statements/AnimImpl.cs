ca01ff86a4d0e000db169c7ba7e95422489d6f86
SubstanceP <3161387475@qq.com>
2025-03-17 14:11:55 +0800

    feat: Nod与Swing动画

--- Statements/AnimImpl.cs
+++ Statements/AnimImpl.cs
@@ -1,14 +1,9 @@
 ﻿using Avalgame.Models;
 using Avalonia.Animation;
-using Avalonia.Animation.Easings;
-using Avalonia.Controls;
 using Avalonia.Media;
 using Avalonia.Styling;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Avalgame.Statements
 {
@@ -24,51 +19,112 @@ namespace Avalgame.Statements
         private static Dictionary<Option, Func<SpriteInfo, Animation>> Anims => anims ??= new()
         {
             { Option.Jump, Jump },
-            //{ Option.Nod, Nod },
-            //{ Option.Swing, Swing },
+            { Option.Nod, Nod },
+            { Option.Swing, Swing },
         };
-        private static Animation Jump(SpriteInfo sprite)
+        private static Animation Jump(SpriteInfo target) => new()
         {
-            return new Animation()
+            Duration = TimeSpan.FromSeconds(.4),
+            IterationCount = new(1),
+            Children =
             {
-                Duration = TimeSpan.FromSeconds(1),
-                IterationCount = new(1),
-                Children =
+                new KeyFrame
                 {
-                    new KeyFrame
+                    Cue = new(0),
+                    Setters =
                     {
-                        Cue = new(0),
-                        Setters =
-                        {
-                            new Setter(TranslateTransform.YProperty, sprite.Rect.Center.Y),
-                        }
-                    },
-                    new KeyFrame
+                        new Setter(TranslateTransform.YProperty, target.Rect.Center.Y),
+                    }
+                },
+                new KeyFrame
+                {
+                    Cue = new(.5),
+                    Setters =
                     {
-                        Cue = new(0.5),
-                        Setters =
-                        {
-                            new Setter(TranslateTransform.YProperty, sprite.Rect.Center.Y - sprite.Rect.Height * .2),
-                        }
-                    },
-                    new KeyFrame
+                        new Setter(TranslateTransform.YProperty, target.Rect.Center.Y - target.Rect.Height * .1),
+                    }
+                },
+                new KeyFrame
+                {
+                    Cue = new(1),
+                    Setters =
                     {
-                        Cue = new(1),
-                        Setters =
-                        {
-                            new Setter(TranslateTransform.YProperty, sprite.Rect.Center.Y),
-                        }
-                    },
-                }
-            };
-        }
-        //private static Animation Nod(Control target)
-        //{
-
-        //}
-        //private static Animation Swing(Control target)
-        //{
-
-        //}
+                        new Setter(TranslateTransform.YProperty, target.Rect.Center.Y),
+                    }
+                },
+            }
+        };
+        private static Animation Nod(SpriteInfo target) => new()
+        {
+            Duration = TimeSpan.FromSeconds(.4),
+            IterationCount = new(2),
+            Children =
+            {
+                new KeyFrame
+                {
+                    Cue = new(0),
+                    Setters =
+                    {
+                        new Setter(TranslateTransform.YProperty, target.Rect.Center.Y),
+                    }
+                },
+                new KeyFrame
+                {
+                    Cue = new(.5),
+                    Setters =
+                    {
+                        new Setter(TranslateTransform.YProperty, target.Rect.Center.Y + target.Rect.Height * .1),
+                    }
+                },
+                new KeyFrame
+                {
+                    Cue = new(1),
+                    Setters =
+                    {
+                        new Setter(TranslateTransform.YProperty, target.Rect.Center.Y),
+                    }
+                },
+            }
+        };
+        private static Animation Swing(SpriteInfo target) => new()
+        {
+            Duration = TimeSpan.FromSeconds(.8),
+            IterationCount = new(2),
+            Children =
+            {
+                new KeyFrame
+                {
+                    Cue = new(0),
+                    Setters =
+                    {
+                        new Setter(TranslateTransform.XProperty, target.Rect.Center.X),
+                    }
+                },
+                new KeyFrame
+                {
+                    Cue = new(.25),
+                    Setters =
+                    {
+                        new Setter(TranslateTransform.XProperty, target.Rect.Center.X + target.Rect.Width * .2),
+                    }
+                },
+                new KeyFrame
+                {
+                    Cue = new(.75),
+                    Setters =
+                    {
+                        new Setter(TranslateTransform.XProperty, target.Rect.Center.X - target.Rect.Width * .2),
+                    }
+                },
+                new KeyFrame
+                {
+                    Cue = new(1),
+                    Setters =
+                    {
+                        new Setter(TranslateTransform.XProperty, target.Rect.Center.X),
+                    }
+                },
+            }
+        };
     }
 }
